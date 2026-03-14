bfd7f221955f817b9757137fbe8aafb642686690
Aditya Yadav <adityashok989@gmail.com>
2026-03-15 00:35:14 +0530

    Update Print.java

--- Basics/Print.java
+++ Basics/Print.java
@@ -1,1 +1,7 @@
-
+public class Print{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("My name is Aditya Yadav");
+        System.out.println("This is my First Program.");
+    }
+}
