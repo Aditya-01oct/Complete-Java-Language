55f4a7595ad48e696dd7520ca0830ba44aa5c7f6
Aditya Yadav <adityashok989@gmail.com>
2026-04-25 22:48:05 +0530

    Clean up whitespace in pattern7 method
    Removed unnecessary whitespace lines in the pattern7 method.

--- Chapter_02_Patterns/Pattern07_PyramidStar.java
+++ Chapter_02_Patterns/Pattern07_PyramidStar.java
@@ -1,19 +1,15 @@
 class Solution {
     public void pattern7(int N) {
         for (int i = 0; i < N; i++) {
-
             for (int j = 0; j < N - i - 1; j++) {
                 System.out.print(" ");
             }
-
             for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
-
             for (int j = 0; j < N - i - 1; j++) {
                 System.out.print(" ");
             }
-
             System.out.println();
         }
     }
